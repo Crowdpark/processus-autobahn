e8862a1253bf3e07040218bdc4495ca1f1d337b9
FrancisVarga <github@varga-net.com>
2012-11-07 11:37:46 +0100

    formating add something

--- example/zeromq/zeromqpush.php
+++ example/zeromq/zeromqpush.php
@@ -11,33 +11,33 @@ echo "=== ZMQ Socket Start===" . PHP_EOL;
 $startTime = microtime(true) . PHP_EOL;
 
 $queue = new \ZMQSocket(new \ZMQContext(), \ZMQ::SOCKET_PUSH, "MySock1");
-$queue->connect("tcp://127.0.0.1:5555");
+$queue->connect("tcp://192.168.42.179:5555");
 
-$endTime  = microtime(true);
+$endTime = microtime(true);
 $execTime = $endTime - $startTime;
 echo "Connection time: " . $execTime . PHP_EOL;
 
 echo "=== ZMQ Socket Connected===" . PHP_EOL;
 
 $startTime = microtime(true);
-$totalMessages = 19000;
+$totalMessages = 190000;
 for ($i = 0; $i <= $totalMessages; $i++) {
 
     $mqData = array(
-        "id"        => 1,
-        "params"    => array(array(
-            "message"  => "This is bullshit to.",
-            "created"  => time(),
+        "id" => 1,
+        "params" => array(array(
+            "message" => "This is bullshit to.",
+            "created" => time(),
             "someData" => mt_rand(0, 49344409875093475),
         )),
-        "method"    => "ZeroMq.TestZeroMq.testEcho",
-        "extended"  => array(),
+        "method" => "ZeroMq.TestZeroMq.testEcho",
+        "extended" => array(),
     );
 
     $queue->send(json_encode($mqData), \ZMQ::MODE_NOBLOCK);
 }
-$endTime  = microtime(true);
+$endTime = microtime(true);
 $execTime = $endTime - $startTime;
 echo "Sending Time: " . $execTime . PHP_EOL;
-
+echo "Total Items sending: " . $totalMessages . PHP_EOL;
 echo "=== ZMQ Socket End===" . PHP_EOL;
